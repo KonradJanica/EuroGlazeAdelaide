c7799d8a0437369c3214bb1f6d39317175a7cd57
Konrad Janica <konradjanica@gmail.com>
2017-10-15 16:20:56 -0700

    fixed scrolling bug

---
+++ index_files/misc.js
@@ -0,0 +1,48 @@
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    if ($elem.height() > $(window).height()) {
+        var diff = $elem.height() - $(window).height();
+        elemTop += (diff + 1) / 2;
+        elemBottom -= (diff + 1) / 2;
+    }
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    $('.project').each(function() {
+        // If the animation has already been started
+        //if ($(this).hasClass('reveal-animation')) return;
+
+        if (isElementInViewport($(this))) {
+            // Start the animation
+            $(this).addClass('reveal-animation');
+        } else {
+            $(this).removeClass('reveal-animation');
+        }
+    });
+}
+
+// Capture scroll events
+var timer;
+$(window).scroll(function(){
+    if (timer) {
+        window.clearTimeout(timer);
+    }
+
+    timer = window.setTimeout(checkAnimation(), 10000);
+});
+
+$(document).ready(function(){
+    checkAnimation();
+});
